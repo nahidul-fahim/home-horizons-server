7bd433363d4e569932b2c4fd7c2276371d051902
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-01-24 09:12:59 +0600

    created api to update a house?

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -15,7 +16,6 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.xeklkbf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,8 +35,34 @@ async function run() {
 
         // Database and collection
         const usersCollection = client.db("horizonHomes").collection("registeredUsers");
+        const allHousesCollection = client.db("horizonHomes").collection("allHouses");
 
 
+
+        // JSON related api
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_WEB_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+
+        // verify token middleware
+        const verifyToken = (req, res, next) => {
+            const tokenAuthorization = req.headers.authorization;
+            if (!tokenAuthorization) {
+                return res.status(401).send({ message: 'Unauthorized' })
+            }
+            const token = tokenAuthorization.split(' ')[1]
+            // verify token
+            jwt.verify(token, process.env.ACCESS_WEB_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'Unauthorized' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
 
 
         // post new user data and duplicate email validation
@@ -52,10 +78,19 @@ async function run() {
         })
 
 
+
+        // post new house data
+        app.post("/newHouse", async (req, res) => {
+            const newHouseData = req.body;
+            const result = await allHousesCollection.insertOne(newHouseData);
+            res.send(result);
+        })
+
+
+
         // get data to login a user
         app.post("/loginUser", async (req, res) => {
             const loginData = req.body;
-            console.log(loginData);
             const email = loginData?.email;
             const password = loginData?.password;
             const query = { email: email };
@@ -66,16 +101,70 @@ async function run() {
             if (existingUser?.password !== password) {
                 return res.send({ login: false, message: "Please recheck your email and password" })
             }
-            res.send({ login: true, existingUser });
+            const userName = existingUser?.name;
+            const userEmail = existingUser?.email;
+            const userRole = existingUser?.userRole;
+            const userId = existingUser?._id;
+            const loggedInUserData = { userName, userEmail, userRole, userId }
+            res.send({ login: true, loggedInUserData });
+        })
+
+
+
+        // get houses for a specific owner
+        app.get("/houseOwnerHouses/:id", async (req, res) => {
+            const houseOwnerId = req.params.id;
+            const query = { houseOwnerId: houseOwnerId };
+            const result = await allHousesCollection.find(query).sort({ _id: -1 }).toArray();
+            res.send(result);
+        })
+
+
+
+        // get a single house
+        app.get("/singleHouse/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await allHousesCollection.findOne(query);
+            res.send(result);
+        })
+
+
+
+        // update a single house
+        app.put("/updateHouse/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDetails = req.body;
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    availabilityDate: updatedDetails.availabilityDate,
+                    bathRooms: updatedDetails.bathRooms,
+                    bedRooms: updatedDetails.bedRooms,
+                    cityName: updatedDetails.cityName,
+                    houseAddress: updatedDetails.houseAddress,
+                    houseDescription: updatedDetails.houseDescription,
+                    houseName: updatedDetails.houseName,
+                    ownerPhone: updatedDetails.ownerPhone,
+                    rentPerMonth: updatedDetails.rentPerMonth,
+                    roomSize: updatedDetails.roomSize,
+                }
+            };
+            const result = await allHousesCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
         })
 
 
 
 
-
-
-
-
+        // delete a house
+        app.delete("/deleteHouse/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await allHousesCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
